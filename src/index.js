e81f67201ebcd2c816d0ad06d8a9b3a190a4e0ad
Steven Li <steven4354@gmail.com>
2018-02-11 19:26:04 -0800

    user creation: added bootstrap for registration login form styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
 
-import './index.css';
+//for bootstrap styling
+import "bootstrap/dist/css/bootstrap.css";
 
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import "./index.css";
+
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import {createLogger} from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const middleware = applyMiddleware(thunk, createLogger());
 export const store = createStore(rootReducer, middleware);
@@ -17,5 +20,5 @@ ReactDOM.render(
   <Provider store={store}>
     <Routes />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
